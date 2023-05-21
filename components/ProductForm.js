fe797c5a928ba55a5bdc3ac5c74fe1a056cbd223
Mustafa Şahin <63456077+mtfsahin@users.noreply.github.com>
2023-05-22 02:24:33 +0300

    update: mongodb database add images

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,23 +1,25 @@
 import axios from "axios";
 import { useState } from "react";
 import { useRouter } from "next/router"
-
+import Loading from "./Loading";
 
 export default function ProductForm({
     _id,
     title: existingTitle,
     description: existingDescription,
     price: existingPrice,
-    images
+    images: existingImage,
 }) {
     const [title, setTitle] = useState(existingTitle || '');
     const [description, setDescription] = useState(existingDescription || '');
     const [price, setPrice] = useState(existingPrice || '');
+    const [images, setImages] = useState(existingImage || []);
     const [goToProducts, setToGoProduct] = useState(false);
+    const [isUploading, setIsUploading] = useState(false);
     const router = useRouter();
 
     async function saveProduct(ev) {
-        const data = { title, description, price }
+        const data = { title , description, price, images }
         ev.preventDefault();
         if (_id) {
             //update
@@ -36,16 +38,17 @@ export default function ProductForm({
     async function uploadImages(ev) {
         const files = ev.target?.files;
         if (files?.length > 0) {
+            setIsUploading(true);
             const data = new FormData;
             for (const file of files){
                 data.append('file', file)
             }
 
-            const res = await fetch('/api/upload',{
-                method:'POST',
-                body:data,
-            })
-            console.log("res:",res);
+            const res = await axios.post('/api/upload', data)
+            setImages(oldImages => {
+                return [...oldImages, ...res.data.links]
+            });
+            setIsUploading(false);
         }
     }
 
@@ -58,6 +61,20 @@ export default function ProductForm({
                 value={title}
                 onChange={ev => setTitle(ev.target.value)} 
             />
+            <label>
+                Photos
+            </label>
+            <div className="flex flex-wrap gap-1 mb-2">
+            {!!images?.length && images.map(link => (
+                <div className="h-24 inline-block">
+                    <img src={link} alt="" className="rounded-lg"/>
+                </div>
+            ))}
+            {isUploading && (
+                <div className="flex items-center h-24">
+                    <Loading/>
+                </div>
+            )}
             <label className="flex flex-col w-24 h-24 cursor-pointer text-center items-center justify-center text-sm gap-1 text-primary rounded-xl bg-white shadow-sm border border-primary">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
@@ -67,6 +84,7 @@ export default function ProductForm({
                 </div>
                 <input type="file" onChange={uploadImages} className="hidden" />
             </label>
+            </div>
             <label htmlFor="">Description</label>
             <textarea
                 placeholder="description"
--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,10 +1,13 @@
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import multiparty from 'multiparty'
-
+import mime from 'mime-types'
+import fs from 'fs';
 
 
 export default async function handle(req, res) {
 
     const form = new multiparty.Form();
+    const BucketName = "next-ecommerce-mustafa";
 
     const { fields, files } = await new Promise((resolve, reject) => {
         form.parse(req, (err, fields, files) => {
@@ -13,13 +16,35 @@ export default async function handle(req, res) {
 
         });
     });
-    console.log('length:', files.file.length)
+    
+    const client= new S3Client({
+        region: 'us-east-1',
+        credentials:{
+            accessKeyId:process.env.S3_ACCESS_KEY,
+            secretAccessKey:process.env.S3_SECRET_ACCESS_KEY
+        }
+    });
+
+    const links = [];
+    for(const file of files.file){
+        const ext = file.originalFilename.split('.').pop();
+        const newFileName = Date.now() + '.' + ext;
+        await client.send(new PutObjectCommand({
+            Bucket:BucketName,
+            Key: newFileName,
+            Body: fs.readFileSync(file.path),
+            ACL: 'public-read',
+            ContentType: mime.lookup(file.path),
+        }));
+        const link = `https://${BucketName}.s3.amazonaws.com/${newFileName}`;
+        links.push(link);
+    }
+
     console.log(fields);
-    return res.json('ok');
+    return res.json({links});
 
 }
 
 export const config = {
     api: { bodyParser: false },
-
 }
