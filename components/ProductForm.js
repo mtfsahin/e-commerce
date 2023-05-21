f43ee29ecc25b587a82e9dcd0f79a759dc4c9079
Mustafa Şahin <63456077+mtfsahin@users.noreply.github.com>
2023-05-21 21:24:33 +0300

    edit: ProductForm component

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,24 +1,31 @@
 import axios from "axios";
 import { useState } from "react";
-import {useRouter} from "next/router"
+import { useRouter } from "next/router"
 
 
 export default function ProductForm({
-    title:existingTitle,
-    description:existingDescription,
-    price:existingPrice
-}){
-    console.log("existingTitle",existingTitle)
+    _id,
+    title: existingTitle,
+    description: existingDescription,
+    price: existingPrice
+}) {
+    console.log("existingTitle", existingTitle)
     const [title, setTitle] = useState(existingTitle || '');
-    const [description, setDescription] = useState(existingDescription || '');
+    const [description, setDescription] = useState(existingDescription || '');
     const [price, setPrice] = useState(existingPrice || '');
     const [goToProducts, setToGoProduct] = useState(false);
     const router = useRouter();
 
-    async function createProduct(ev) {
-        ev.preventDefault();
+    async function saveProduct(ev) {
         const data = { title, description, price }
-        await axios.post('/api/products', data);
+        ev.preventDefault();
+        if (_id) {
+            //update
+            await axios.put('/api/products', { ...data, _id });
+        } else {
+            //create
+            await axios.post('/api/products', data);
+        }
         setToGoProduct(true);
     }
 
@@ -27,25 +34,25 @@ export default function ProductForm({
     }
 
     return (
-        <form onSubmit={createProduct}>
-                <label htmlFor="">Product Name</label>
-                <input
-                    type="text"
-                    placeholder="product name"
-                    value={title}
-                    onChange={ev => setTitle(ev.target.value)} />
-                <label htmlFor="">Description</label>
-                <textarea
-                    placeholder="description"
-                    value={description}
-                    onChange={ev => setDescription(ev.target.value)} />
-                <label htmlFor="">Price(₺)</label>
-                <input
-                    type="number"
-                    placeholder="price"
-                    value={price}
-                    onChange={ev => setPrice(ev.target.value)} />
-                <button type="submit" className="btn-primary">Save</button>
+        <form onSubmit={saveProduct}>
+            <label htmlFor="">Product Name</label>
+            <input
+                type="text"
+                placeholder="product name"
+                value={title}
+                onChange={ev => setTitle(ev.target.value)} />
+            <label htmlFor="">Description</label>
+            <textarea
+                placeholder="description"
+                value={description}
+                onChange={ev => setDescription(ev.target.value)} />
+            <label htmlFor="">Price(₺)</label>
+            <input
+                type="number"
+                placeholder="price"
+                value={price}
+                onChange={ev => setPrice(ev.target.value)} />
+            <button type="submit" className="btn-primary">Save</button>
         </form>
     )
 }
